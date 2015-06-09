54d87475e2875bebb9525f7a94b371f38e22d214
DevTopic <pasquale@somewhere.com>
2015-06-09 12:10:04 +0100

    Alternative to recursion is folding

--- src/main/scala/example/ListsAlternativeImplementation.scala
+++ src/main/scala/example/ListsAlternativeImplementation.scala
@@ -23,7 +23,9 @@ object ListsAlternative {
   * @param xs A list of natural numbers
   * @return The sum of all elements in `xs`
   */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = {
+      xs.foldLeft(0)(_+_)
+    }
   
   /**
   * This method returns the largest element in a list of integers. If the
@@ -38,5 +40,11 @@ object ListsAlternative {
   * @return The largest element in `xs`
   * @throws java.util.NoSuchElementException if `xs` is an empty list
   */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      if (xs.isEmpty) {throw new NoSuchElementException} else {
+        if (xs.tail.isEmpty){ xs.head } else {
+          xs.tail.foldLeft(xs.head)(Math.max(_, _))
+        }
+      }
+    }
   }
